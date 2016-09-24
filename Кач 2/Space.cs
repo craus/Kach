83834163131f1e21b6cf82b29cc925585cdd264a
craus <craus902@gmail.com>
2016-09-24 23:59:17 +0400

    more transparent labels; removed reputation label; release zip renewed

--- Кач 2/Space.cs
+++ Кач 2/Space.cs
@@ -34,12 +34,12 @@ namespace Кач_2
         public override void paint(System.Drawing.Graphics g)
         {
             base.paint(g);
-            com.print(g, com.transparent(Color.White,0.5), 20, 20, String.Format("Время: {0}", (int)time));
-            com.print(g, com.transparent(Color.White, 0.5), 20, 40, String.Format("Счёт: {0}", (int)totalCost));
-            com.print(g, com.transparent(Color.White, 0.5), 20, 60, String.Format("Смертей: {0}", (int)deathCount));
-            com.print(g, com.transparent(Color.White, 0.5), 20, 80, String.Format("Репутация: {0}", (int)reputation));
-            com.print(g, com.transparent(Color.White, 0.5), 20, 100, String.Format("Родилось: {0}", (int)bornMass));
-            com.print(g, com.transparent(Color.White, 0.5), 20, 120, String.Format("Умерло: {0}", (int)deadMass));
+            com.print(g, com.transparent(Color.White,0.95), 20, 20, String.Format("Время: {0}", (int)time));
+            com.print(g, com.transparent(Color.White, 0.95), 20, 40, String.Format("Счёт: {0}", (int)totalCost));
+            com.print(g, com.transparent(Color.White, 0.95), 20, 60, String.Format("Смертей: {0}", (int)deathCount));
+            //com.print(g, com.transparent(Color.White, 0.95), 20, 80, String.Format("Репутация: {0}", (int)reputation));
+            com.print(g, com.transparent(Color.White, 0.95), 20, 100, String.Format("Родилось: {0}", (int)bornMass));
+            com.print(g, com.transparent(Color.White, 0.95), 20, 120, String.Format("Умерло: {0}", (int)deadMass));
             if (!play) com.print(g, com.transparent(Color.Silver), size.Width / 2, size.Height / 2, "Пауза", 400, StringAlignment.Center, StringAlignment.Center); 
         }
 
